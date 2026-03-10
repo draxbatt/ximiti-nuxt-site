cc7feccc09515be2bfdb55c26500118b9f705dfc
Drax <drix@example.com>
2026-03-10 14:22:56 +0100

    feat(site): add page for issue 27

---
+++ pages/add-favicon-and-manifest.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container"> 
+    <h1>Add favicon and manifest</h1>
+    <p>Add favicon.ico and webmanifest, update head metadata</p>
+  </div>
+</template>
+
+<style>
+.container { max-width: 900px; margin: 40px auto; }
+</style>
