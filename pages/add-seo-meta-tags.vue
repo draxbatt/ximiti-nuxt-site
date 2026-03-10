7f6ac586e4d822ed2e95f29d99ef8ca4e99a68a5
Drax <drix@example.com>
2026-03-10 12:23:05 +0100

    feat(site): add page for issue 13

---
+++ pages/add-seo-meta-tags.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container"> 
+    <h1>Add SEO & meta tags</h1>
+    <p>Ajouter balises meta, title, description et sitemap</p>
+  </div>
+</template>
+
+<style>
+.container { max-width: 900px; margin: 40px auto; }
+</style>
