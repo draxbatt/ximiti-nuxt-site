046c34ab3ab43cb83b125185a115536694f6c7b8
Drax <drix@example.com>
2026-03-10 12:00:16 +0100

    feat(site): add page for issue 10

---
+++ pages/add-hero-header-section.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container"> 
+    <h1>Add hero & header section</h1>
+    <p>Créer la section d'accueil avec titre, sous-titre et CTA</p>
+  </div>
+</template>
+
+<style>
+.container { max-width: 900px; margin: 40px auto; }
+</style>
